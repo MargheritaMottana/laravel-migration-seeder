704004e85be74d4e708f23ee03589e4829424877
MargheritaMottana <margherita.mottana@gmail.com>
2024-10-22 16:14:36 +0200

    - cancellata la migration di prova - creata la migration con tutti i dati, aggiunto anche l'id

--- database/migrations/2024_10_21_193148_create_trains_table.php
+++ database/migrations/2024_10_21_193148_create_trains_table.php
@@ -13,6 +13,26 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+
+            //Azienda
+            $table->string('company', 64);            
+            //Stazione di partenza
+            $table->string('dep_station', 64);
+            //Stazione di arrivo
+            $table->string('arr_station', 64);
+            //Orario di partenza
+            $table->string('dep_time');
+            //Orario di arrivo
+            $table->string('arr_time');
+            //Codice Treno
+            $table->string('code', 16)->nullable();
+            //Numero Carrozze
+            $table->tinyInteger('wagons_number')->nullable()->unsigned();
+            //In orario
+            $table->boolean('on_time')->default(true);
+            //Cancellato
+            $table->boolean('canceled')->default(false);
+
             $table->timestamps();
         });
     }
